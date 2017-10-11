356af9ccdafa2b99a12685a6d244a456685a6f87
Jeff2310 <jeff23101719@gmail.com>
2017-10-11 22:55:04 +0800

    改进下游戏体验...

--- casino.cpp
+++ casino.cpp
@@ -1,90 +1,95 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
-#include <string.h>
+#include <cstring>
+#include <random>
+#include <unistd.h>
 
-//TODO:尝试把文字改成Unicode符号
 const char suit[4][5]={"梅花","红桃","方片","黑桃"};
 //TODO:把T改为10
-const char number[14]={'0','A','2','3','4','5','6','7','8','9','T','J','Q','K'};
-enum choice{YES = 0, NO = 1, QUIT = 2, UNDEFINED = 3};
+const char *number[14]={"0","A","2","3","4","5","6","7","8","9","10","J","Q","K" };
+enum choice{YES = 0, NO = 1, QUIT = 2};
 
 struct Card{
     int score;
-    char num;
+    const char *num;
     const char *suit;
-    Card():score(0),num(number[0]),suit(NULL){}
+    Card():score(0),num(NULL),suit(NULL){}
 }card[52];
 
-//TODO:自定义洗多少副牌
 void initCard(){
-    for(int i=0; i<4; i++){
-        for(int j=1; j<=13; j++){
-            Card *tmp = &card[i*13+j-1];
-            tmp->suit=suit[i];
-            if(j==1)
+    for (int i = 0; i < 4; i++) {
+        for (int j = 1; j <= 13; j++) {
+            Card *tmp = &card[+i * 13 + j - 1];
+            tmp->suit = suit[i];
+            if (j == 1)
                 tmp->score = 11;
-            else if(j<=10)
-                tmp->score= j;
+            else if (j <= 10)
+                tmp->score = j;
             else
-                tmp->score= 10;
+                tmp->score = 10;
             tmp->num = number[j];
         }
     }
 }
 
-void shuffle(Card *c, int n){
+void shuffle(Card *arr, int n, std::default_random_engine *e){
     Card tmp;
     for(int i=0; i<n; i++){
-        int index = rand()%(n-i)+i; //TODO:改成c++版本随机数
+        int index = (*e)()%(n-i)+i; //TODO:改成c++版本随机数
         if(i!=index) {
-            tmp = card[i];
-            card[i] = card[index];
-            card[index] = tmp;
+            tmp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = tmp;
         }
     }
 }
 
-//TODO: 改为需要检查输入重新询问
 choice getChoice() {
-    printf("(y/n/q?)");
     char str[100];
-    scanf("%s", str);
-    if (str[1]){
-        printf("请您检查输入\n");
-        return UNDEFINED;
-    }
-    switch(str[0]) {
-        case 'y':
-            printf("OK\n");
-            return YES;
-        case 'n':
-            printf("Not Bad\n");
-            return NO;
-        case 'q':
-            printf("Bye\n");
-            return QUIT;
-        default:
+    while(true) {
+        printf("(y/n/q?)");
+        scanf("%s", str);
+        if (str[1]) {
             printf("请您检查输入\n");
-            return UNDEFINED;
+            continue;
+        }
+        switch (str[0]) {
+            case 'y':
+                printf("OK\n");
+                return YES;
+            case 'n':
+                printf("Not Bad\n");
+                return NO;
+            case 'q':
+                printf("Bye\n");
+                return QUIT;
+            default:
+                printf("请您检查输入\n");
+                continue;
+        }
     }
 }
 
+
+//展示手牌
 void displayHand(Card *hand, int n,int score){
     if(!n) return;
     putchar('#');
     for(int i=0; i<n; i++){
-        printf("【%s%c】 ",hand[i].suit,hand[i].num);
+        printf("【%s%s】 ",hand[i].suit,hand[i].num);
     }
+    //把最后多出来的一个空格删掉
     printf("\b#\n共计%d点\n",score);
 }
 
 void drawCard(Card *hand, int *size, int *score, int *index){
     hand[(*size)++]=card[*index];
-    printf("您获得一张【%s%c】\n", card[*index].suit, card[*index].num);
+    printf("您获得一张【%s%s】\n", card[*index].suit, card[*index].num);
     *score+=card[*index].score;
     (*index)++;
     displayHand(hand,*size,*score);
+    sleep(1);
 }
 
 int main() {
@@ -92,13 +97,13 @@ int main() {
     int handSize[2],score[2];
     //flag 0=初始, 1=退出, 2=爆牌, 3=等待拼点, 4=拼点
     int flag, index, turn;
-    srand(time(NULL));
+    std::default_random_engine e((unsigned)time(NULL));
+    printf("澳门首家线上赌场上线啦！性感荷官在线发牌。\n21点游戏惊险刺激\n");
     initCard();
     while(true){
-        //TODO: 加点骚话
         printf("游戏开始\n");
-        printf("玩家 %d 为庄主\n", rand()%2+1);
-        shuffle(card,52);
+        printf("玩家 %d 为庄主\n", e()%2+1);
+        shuffle(card,52, &e);
         //displayHand(card,52,0); //debug
         memset(hand, 0, sizeof(hand));
         memset(handSize, 0, sizeof(handSize));
@@ -119,7 +124,7 @@ int main() {
                 if(c==YES){
                     drawCard(hand[i], &handSize[i], &score[i], &index);
                     if(score[i]>21){
-                        printf("玩家 %d 爆牌，出局\n", i+1);
+                        printf("玩家 %d 爆牌，出局， 玩家 %d 获胜\n", i+1, 2-i);
                         flag = 2;
                         break;
                     }
@@ -127,6 +132,7 @@ int main() {
                     if(i==0) flag = 3;
                     else
                         if(flag == 3) flag = 4;
+                    sleep(1);
                 }else{
                     flag = 1;
                     break;
@@ -144,7 +150,10 @@ int main() {
                 }
             }
         }
+        printf("游戏结束\n");
+        sleep(1);
         if(flag==1) break;
+        printf("重新开始游戏\n============\n");
     }
     return 0;
 }
